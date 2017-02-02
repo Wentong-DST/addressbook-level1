2b8ab0ccdaf413ea43fab7172ae215a1b90b2f29
Wentong <liwentongnus@gmail.com>
2017-02-02 14:26:40 +0800

    Excercise on SLAP

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -206,16 +206,35 @@ public class AddressBook {
      * ====================================================================
      */
 
+	 
     public static void main(String[] args) {
         showWelcomeMessage();
-        processProgramArgs(args);
-        loadDataFromStorage();
-        while (true) {
-            String userCommand = getUserInput();
-            echoUserCommand(userCommand);
-            String feedback = executeCommand(userCommand);
-            showResultToUser(feedback);
+
+		// Ex1: reduce SLAP of method of processProgramArgs
+        if (args.length >= 2) {
+            showToUser(MESSAGE_INVALID_PROGRAM_ARGS);
+            exitProgram();
         }
+
+        if (args.length == 1) {
+            setupGivenFileForStorage(args[0]);
+        }
+
+        if(args.length == 0) {
+            setupDefaultFileForStorage();
+        }
+    }
+        loadDataFromStorage();
+		userInputInstructions();
+		
+		// Ex2: refactor the code
+		private static void userInputInstructions() {
+			while (true) {
+				 String userCommand = getUserInput();
+				 echoUserCommand(userCommand);
+				 String feedback = executeCommand(userCommand);
+				 showResultToUser(feedback);
+			}
     }
 
     /*
